ec27abde04837e17db8772de2bddabd8fe04fd2c
JotaDD <joaoeduardogomes@gmail.com>
2024-09-10 16:35:45 -0300

    chore: add db seed

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,13 +2,14 @@ import { timestamp } from 'drizzle-orm/pg-core'
 import { text } from 'drizzle-orm/pg-core'
 import { pgTable } from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
+import { integer } from 'drizzle-orm/pg-core'
 
 export const goals = pgTable('goals', {
   id: text('id')
     .primaryKey()
     .$defaultFn(() => createId()),
   title: text('title').notNull(),
-  desiredWeeklyFrequency: text('desired_weekly_frequency').notNull(),
+  desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
