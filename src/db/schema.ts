05b4244a1cf49b7e6a378b64e2973e377b29b895
JotaDD <joaoeduardogomes@gmail.com>
2024-09-10 16:03:31 -0300

    chore: add goals and goals completions table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,26 @@
 import { timestamp } from 'drizzle-orm/pg-core'
 import { text } from 'drizzle-orm/pg-core'
 import { pgTable } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
 
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
   title: text('title').notNull(),
   desiredWeeklyFrequency: text('desired_weekly_frequency').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const goalCompletions = pgTable('goal_completions', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  goalId: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
