d07789571b8dfd437a9233b8a2b88744344a59dc
JotaDD <joaoeduardogomes@gmail.com>
2024-09-10 22:04:39 -0300

    feat: implement CreateGoalCompletion

--- src/functions/create-goal-completion.ts
+++ src/functions/create-goal-completion.ts
@@ -1,5 +1,7 @@
+import { sql, eq } from 'drizzle-orm'
 import { db } from '../db'
-import { goalCompletions } from '../db/schema'
+import { goalCompletions, goals } from '../db/schema'
+import { goalCompletionCounts } from './helpers/sql/goal-completion-counts'
 
 interface CreateGoalCompletionRequest {
   goalId: string
@@ -8,9 +10,30 @@ interface CreateGoalCompletionRequest {
 export async function createGoalCompletion({
   goalId,
 }: CreateGoalCompletionRequest) {
-  const result = await db.insert(goalCompletions).values({ goalId }).returning()
+  const result = await db
+    .with(goalCompletionCounts)
+    .select({
+      desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+      completionCount: sql /*sql*/`
+        COALESCE(${goalCompletionCounts.completionCount}, 0 ) AS completionCount  -- if statement no sql
+        `.mapWith(Number),
+    })
+    .from(goals)
+    .leftJoin(goalCompletionCounts, eq(goalCompletionCounts.goalId, goals.id))
+    .where(eq(goals.id, goalId))
+    .limit(1)
 
-  const goalCompletion = result[0]
+  const { completionCount, desiredWeeklyFrequency } = result[0]
+
+  if (completionCount >= desiredWeeklyFrequency) {
+    throw new Error('Goal already completed this week!')
+  }
+
+  const insertResult = await db
+    .insert(goalCompletions)
+    .values({ goalId })
+    .returning()
+  const goalCompletion = insertResult[0]
 
   return {
     goalCompletion,
