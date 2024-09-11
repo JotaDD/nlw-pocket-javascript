eedd50ff71591de6ddf7210e1542a140ea8b6c2e
JotaDD <joaoeduardogomes@gmail.com>
2024-09-10 22:48:39 -0300

    refactor: set goalsCreatedUpToWeek as helper

--- src/functions/get-week-pending-goals.ts
+++ src/functions/get-week-pending-goals.ts
@@ -1,22 +1,9 @@
-import { lte, eq, sql } from 'drizzle-orm'
+import { eq, sql } from 'drizzle-orm'
 import { db } from '../db'
-import { goals } from '../db/schema'
-import { lastDayOfWeek } from './helpers/dayjs'
 import { goalCompletionCounts } from './helpers/sql/goal-completion-counts'
+import { goalsCreatedUpToWeek } from './helpers/sql/goals-created-up-to-week'
 
 export async function getWeekPendingGoals() {
-  const goalsCreatedUpToWeek = db.$with('goals_created_up_to_week').as(
-    db
-      .select({
-        id: goals.id,
-        title: goals.title,
-        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
-        createdAt: goals.createdAt,
-      })
-      .from(goals)
-      .where(lte(goals.createdAt, lastDayOfWeek))
-  )
-
   const pendingGoals = await db
     .with(goalsCreatedUpToWeek, goalCompletionCounts)
     .select({
