4a51e396eb66ace04900759fbb2dc0d43409eb9f
JotaDD <joaoeduardogomes@gmail.com>
2024-09-10 22:26:16 -0300

    refactor: add route files at server

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,61 +4,19 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { createGoal } from '../functions/create-goals'
-import z from 'zod'
-import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
-import { createGoalCompletion } from '../functions/create-goal-completion'
+import { createGoalRoute } from './routes/create-goal'
+import { createCompletionRoute } from './routes/create-goal-completion'
+import { getPendingGoalsRoute } from './routes/get-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-const createGoalSchema = z.object({
-  title: z.string(),
-  desiredWeeklyFrequency: z.number().int().min(1).max(7),
-})
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
 
-const createGoalCompletionsSchema = z.object({
-  goalId: z.string(),
-})
-
-app.get('/pending-goals', async () => {
-  const { pendingGoals } = await getWeekPendingGoals()
-
-  return { pendingGoals }
-})
-
-app.post(
-  '/goals',
-  {
-    schema: {
-      body: createGoalSchema,
-    },
-  },
-  async request => {
-    const { title, desiredWeeklyFrequency } = request.body
-
-    await createGoal({
-      title: title,
-      desiredWeeklyFrequency: desiredWeeklyFrequency,
-    })
-  }
-)
-
-app.post(
-  '/completions',
-  {
-    schema: {
-      body: createGoalCompletionsSchema,
-    },
-  },
-  async request => {
-    const { goalId } = request.body
-
-    await createGoalCompletion({ goalId })
-  }
-)
 app
   .listen({
     port: 3333,
