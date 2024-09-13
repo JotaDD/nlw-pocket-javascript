ce019fc6ca4503737c6e3e7e7cb094268f2eea33
JotaDD <joaoeduardogomes@gmail.com>
2024-09-12 23:23:40 -0300

    feat(web): add dayjs getTimefromDate and getFormattedDate functions

--- web/src/utils/dayjs.ts
+++ web/src/utils/dayjs.ts
@@ -5,3 +5,15 @@ dayjs.locale(ptBR)
 
 export const firstDayOfWeek = dayjs().startOf('week').format('D MMM')
 export const lastDayOfWeek = dayjs().endOf('week').format('D MMM')
+
+export function getWeekDayByDate(date: dayjs.ConfigType) {
+  return dayjs(date).format('dddd')
+}
+
+export function getFormattedDate(date: dayjs.ConfigType) {
+  return dayjs(date).format('D[ de ]MMMM')
+}
+
+export function getTimefromDate(date: dayjs.ConfigType) {
+  return dayjs(date).format('HH:mm')
+}
